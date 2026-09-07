1d4158b75da26749f23e861703f7564c20e718eb
Davexh <davesh.backup@gmail.com>
2026-09-07 14:39:39 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,11 +8,11 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.batteryrestrict"
+        applicationId = "com.android.ampify"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 69
+        versionName = "6.9"
     }
 
     buildFeatures {
