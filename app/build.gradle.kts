58e09eb3c7d7a3efd8a3f98cf0cbdbf4ae607437
Davexh <davesh.backup@gmail.com>
2026-09-07 09:28:28 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("com.google.android.material:material:1.12.0")
 
     // Root shell library
     implementation("com.github.topjohnwu.libsu:core:6.0.0")
