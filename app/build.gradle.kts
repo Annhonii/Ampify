f3d741c303afeb0eb9aef20e6df75ab8b88eb487
Davexh <davesh.backup@gmail.com>
2026-09-09 16:01:53 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,8 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
 android {
     namespace = "com.example.batteryrestrict"
     compileSdk = 34
@@ -35,4 +40,19 @@ android {
             output.outputFileName = "Ampify.apk"
         }
     }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation(platform("androidx.compose:compose-bom:2024.06.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.animation:animation")
+    implementation("com.google.android.material:material:1.12.0")
+
+    // Root shell library
+    implementation("com.github.topjohnwu.libsu:core:6.0.0")
 }
